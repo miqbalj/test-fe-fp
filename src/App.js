25b02e1b0d6b2db58e1abc002de0b6db8ba11060
Miqbal <miqbal.j00@gmail.com>
2022-06-19 11:48:27 +0700

    edit layout on login and register page

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
-      {/* <Login /> */}
+      {/* <Navbar /> */}
+      <Login />
       {/* <Register /> */}
     </div>
   );
